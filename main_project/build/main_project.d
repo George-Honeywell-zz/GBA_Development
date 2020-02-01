ffd05ff26bea1b7c072dddda10309e3a94e2f588
George-Honeywell <UP843814@myport.ac.uk>
2020-02-01 16:46:39 +0000

    Initial Project Commit.

---
+++ main_project/build/main_project.d
@@ -0,0 +1,20 @@
+main_project.o: \
+ C:/Users/George/Desktop/GBA_Development/main_project/source/main_project.c \
+ C:/devkitPro/libgba/include/gba_console.h \
+ C:/devkitPro/libgba/include/gba_base.h \
+ C:/devkitPro/libgba/include/gba_types.h \
+ C:/devkitPro/libgba/include/gba_video.h \
+ C:/devkitPro/libgba/include/gba_interrupt.h \
+ C:/devkitPro/libgba/include/gba_systemcalls.h
+
+C:/devkitPro/libgba/include/gba_console.h:
+
+C:/devkitPro/libgba/include/gba_base.h:
+
+C:/devkitPro/libgba/include/gba_types.h:
+
+C:/devkitPro/libgba/include/gba_video.h:
+
+C:/devkitPro/libgba/include/gba_interrupt.h:
+
+C:/devkitPro/libgba/include/gba_systemcalls.h:
---
+++ main_project/source/assembler.s
@@ -0,0 +1,26 @@
+.THUMB				@ turn on thumb
+.ALIGN  2			@ align code correctly for GBA
+.GLOBL  myfunc		@ name of function goes here
+
+
+
+.THUMB_FUNC			@ we are about to declare a thumb function
+myfunc:				@ function start
+
+push { r4-r7, lr }	@ push r4-r7 and link register onto stack. Your function might use these
+					@ registers, so we need to preserve the values just in case!
+					@ we don't need to worry about r0-r3 as it is assumed they will be regularly messed up anyway
+	
+
+@ the magic happens here!
+@ r0-r3 will automatically contain the parameters sent when calling the function.
+
+	ldr r4, [r0]		@ hopefully whichever function called this left a memory address in r0 - dereference value into r4
+	add r4, #1			@ add 1 to value in r4
+	str r4, [r0] 		@ store data in r4 at r0 address
+
+pop { r4-r7 }		@ pop first 4 values from stack back into r4-r7, and also
+pop { r3 }			@ pop the next value from stack (stored value for lr) into some unused register, e.g. r3 -
+					@ we cannot overwrite lr so we have to do it via a normal register
+bx r3				@ "branch and exchange" (return) back to C, using the previous value for lr stored in r3
+@ ==================================
