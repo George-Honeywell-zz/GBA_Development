44624c63d22544716e0f42eb3db58f78422ea257
George-Honeywell <34190137+George-Honeywell@users.noreply.github.com>
2020-02-25 12:28:42 +0000

    Using Binary & Bitshifting to change the values of each tile.

--- tile_design/source/tile_design.c
+++ tile_design/source/tile_design.c
@@ -31,7 +31,10 @@ int main(void) {
 	*/
 	unsigned int* ptr = (unsigned int*)0x6000c20; //'a'
 	//ptr[1] = 0001; 
-	ptr[(4 * 4) + 4] = 0001;
+
+	//0bXXXX can be used to change the colour value of each Pixel.
+	//This is done from RIGHT to LEFT.
+	ptr[4] = 0b00010000000100000001000000010000;
 
 	// main loop
 	while (1) 
@@ -42,7 +45,7 @@ int main(void) {
 
 		// ansi escape sequence to set print co-ordinates
 		// /x1b[line;columnH
-		iprintf("\x1b[10;0Ha b c d e f g h i j\nk l m n o p q r\ns t u v w x y z");
+		iprintf("\x1b[2;0Ha b c d e f g h i j\nk l m n o p q r\ns t u v w x y z");
 
 
 
