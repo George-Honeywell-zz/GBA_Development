87d4aca7a2dfd0b8905813a90b543237b2ec4a38
George-Honeywell <UP843814@myport.ac.uk>
2020-03-16 17:38:54 +0000

    Working on GBA Input.

--- input/source/input.c
+++ input/source/input.c
@@ -7,7 +7,7 @@
 #include <../../../../../devkitPro/libgba/include/gba_input.h>
 #include <stdio.h>
 
-
+#define KEY_MASK 0x03FF
 
 //---------------------------------------------------------------------------------
 // Program entry point
@@ -23,19 +23,40 @@ int main(void) {
 	consoleDemoInit();
 
 	//Controller Input
-	scanKeys();
-	u16 button;
-	
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	u16 buttonPress = 0;
 
 
-	
 	// main loop
 	while (1) 
 	{
 		VBlankIntrWait();
-
-		if (keysHeld(KEY_A & button)) {
-			iprintf("\x1b[2;0HA Key Pressed.");
+		scanKeys();
+		if (keysDown(1)) {
+			iprintf("\x1b[0;0HKeys Down.");
+			iprintf("\x1b[1;0HZ(a) Key Pressed. %d", a);
+			a++;
+		}
+		iprintf("\x1b[2;0H~~~~~~~~~~~~~~~");
+		if (keysDownRepeat(2)) {
+			iprintf("\x1b[3;0HKeys Down Repeat.");
+			iprintf("\x1b[4;0HZ(a) Key Pressed. %d", b);
+			b++;
+		}
+		iprintf("\x1b[5;0H~~~~~~~~~~~~~~~");
+		if (keysUp(4)) {
+			iprintf("\x1b[6;0HKeys Up.");
+			iprintf("\x1b[7;0HZ(a) Key Pressed. %d", c);
+			c++;
+		}
+		iprintf("\x1b[8;0H~~~~~~~~~~~~~~~");
+		if (keysHeld(8)) {
+			iprintf("\x1b[9;0HKeys Held.");
+			iprintf("\x1b[10;0HZ(a) Key Pressed. %d", d);
+			d++;
 		}
 	}
 }
